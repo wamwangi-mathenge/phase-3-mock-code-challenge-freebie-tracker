69a5362b57447e13d090533e6315a34d3a16e49b
Brian <wamwangimathenge@gmail.com>
2023-03-02 12:52:43 +0300

    Freebie association methods

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -3,4 +3,12 @@ class Freebie < ActiveRecord::Base
     belongs_to :company
     belongs_to :dev
 
+    def dev
+        Dev.find(self.dev_id)
+    end
+
+    def company
+        Company.find(self.company_id)
+    end
+
 end
